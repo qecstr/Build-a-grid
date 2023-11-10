481ef53fd98a27773db8a4c6bd9de551b22f11d2
qecstr <yessenov.zhassulan@gmail.com>
2023-11-10 20:27:28 +0300

    changing list to grid trying to create good grid but paddings are not good, created box row and column to fixate objects in card

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,18 +5,26 @@ import android.provider.ContactsContract.Data
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -50,32 +58,48 @@ fun GridApp(){
 
 }
 @Composable
-fun GripCard(topic: Topic, modifier: Modifier = Modifier){
-    Card (modifier = modifier){
-            Column {
+fun GripCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Row {
+            Box {
                 Image(
                     painter = painterResource(id = topic.imageRes),
                     contentDescription = stringResource(id = topic.name),
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .height(194.dp),
+                        .size(68.dp,68.dp)
+                        .aspectRatio(1f),
                     contentScale = ContentScale.Crop
                 )
-                Text(
-                    text = LocalContext.current.getString(topic.name),
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.headlineSmall
-                )
             }
+                Column {
+                    Text(
+                        text = LocalContext.current.getString(topic.name),
+                        modifier = Modifier.padding(16.dp,16.dp,16.dp,8.dp),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = topic.availableCourses.toString(),
+                            style = MaterialTheme.typography.labelMedium,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+                }
+            }
+        }
     }
-}
+
 @Composable
 fun coursesGrid(topicList: List<Topic>,modifier: Modifier = Modifier){
-    LazyColumn(modifier = modifier){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ){
         items(topicList){topic ->
             GripCard(
                 topic = topic,
-                modifier = Modifier.padding(8.dp)
             )
 
         }
